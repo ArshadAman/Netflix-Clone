f8e9d283e11635377ba092804d62e27d812bb1bd
ArshadAman <arshadaman202@gmail.com>
2022-10-10 21:38:05 +0530

    Removed firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyClsvHZ3352cXHjlKgwnIH9AD06n-UXBB8",
-    authDomain: "netflix-clone2422.firebaseapp.com",
-    projectId: "netflix-clone2422",
-    storageBucket: "netflix-clone2422.appspot.com",
-    messagingSenderId: "1058879402512",
-    appId: "1:1058879402512:web:0277585d0e86a9ce52d095"
+      // Your firebase configurations
   };
 
 const firebaseApp = initializeApp(firebaseConfig);
